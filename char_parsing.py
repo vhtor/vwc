a8ead4c7e2bc3b23951ef8e5d9520613fb4d96db
vhtor <victoreverend@gmail.com>
2024-09-07 18:04:26 -0300

    - Added the possibility to read from standard input just like wc

--- char_parsing.py
+++ char_parsing.py
@@ -1,14 +1,17 @@
+import sys
+
 def count_chars(file_name):
-  try:
-    with open(file_name, "r", encoding="utf-8") as file:
-      char_count = 0
-      buffer_size = 1024 * 1024
-      buffer = file.read(buffer_size)
-      
-      while buffer:
-        char_count += len(buffer)
-        buffer = file.read(buffer_size)
-        
-      return f"Char count: {char_count}\n"
-  except:
-    return ""
+    try:
+        with open(file_name, "r", encoding="utf-8") as file:
+            buffer_size = 1024 * 1024
+            char_count = 0
+            buffer = file.read(buffer_size)
+            
+            while buffer:
+                char_count += len(buffer)
+                buffer = file.read(buffer_size)
+            
+            return f"Char count: {char_count}\n"
+    except Exception as e:
+        print(f"Error reading file: {e}", file = sys.stderr)
+        return ""
--- vwc.py
+++ vwc.py
@@ -1,4 +1,7 @@
 import argparse
+import sys
+import tempfile
+import os
 
 from byte_parsing import count_bytes
 from line_parsing import count_lines
@@ -9,19 +12,31 @@ from char_parsing import count_chars
 parser = argparse.ArgumentParser(description = "vwc: Custom command-line word count")
 
 # Defining available Arguments
-parser.add_argument("-c", "--bytes", action = "store_true", help = "Count bytes")
+parser.add_argument("-b", "--bytes", action = "store_true", help = "Count bytes")
 parser.add_argument("-l", "--lines", action = "store_true", help = "Count lines")
 parser.add_argument("-w", "--words", action = "store_true", help = "Count words")
-parser.add_argument("-m", "--chars", action = "store_true", help = "Count characters")
-parser.add_argument("filename", type = str, help = "File to process")
+parser.add_argument("-c", "--chars", action = "store_true", help = "Count characters")
+parser.add_argument("filename", nargs = '?', type = str, help = "File to process")
 
 # Parsing Arguments
 args = parser.parse_args()
 is_default_parsing = not (args.bytes or args.lines or args.words or args.chars)
 
-# Parsing file
-file_name = args.filename
-
+# If a file is specified, open the file for reading
+if args.filename:
+  file_name = args.filename
+# If no file is specified, read from standard input
+elif sys.stdin.isatty():
+  print("No file or input from pipe was provided.")
+  sys.exit(1)
+else:
+  with tempfile.NamedTemporaryFile(delete = False, mode = "w") as temp_file:
+    file_name = temp_file.name
+    
+    # Write stdin data to the temporary file
+    for line in sys.stdin:
+        temp_file.write(line)
+    
 # The string with the all info about the file to be displayed to the user
 final_result = ""
 
@@ -38,7 +53,10 @@ if args.bytes or is_default_parsing:
 if args.chars:
   char_count = count_chars(file_name)
   final_result += char_count if char_count != "" else ""
-
+  
+# Clean up the temporary file if one was created
+if not args.filename:
+    os.remove(file_name)
 
 # Display the final result
 print(final_result)
